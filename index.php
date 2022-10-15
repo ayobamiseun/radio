004299abb1fe3a28f09ba037b5dead47b7d01c19
ayobamiseun <66267222+ayobamiseun@users.noreply.github.com>
2022-10-15 16:51:33 +0100

    error fix

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
     <meta name="twitter:image" content="<?php echo $image; ?>">
 
 
-<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.2/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" type="text/css" href="styles/main_styles.css">
